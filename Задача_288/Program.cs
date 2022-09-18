1bbe36ff0e5dd2515485d6d425293777ab5b1ccd
Kseniya <pashkina.ks@mail.ru>
2022-09-18 23:04:21 +0700

    Задача_28

---
+++ Задача_288/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 , 5 -> 120
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Факториал равен {FindProduce()}");
+
+int FindProduce()
+{
+    int produce = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        produce = produce * i;
+    }
+    return produce;
+}
